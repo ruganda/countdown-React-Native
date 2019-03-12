16f9005f0fae7435aa958143e097e08133a525de
ruganda <mubaruganda@gmail.com>
2019-03-12 11:10:28 +0300

    Add links screen

--- countdown/screens/LinksScreen.js
+++ countdown/screens/LinksScreen.js
@@ -13,7 +13,7 @@ export default class LinksScreen extends React.Component {
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
         <ExpoLinksView />
-      </ScrollView>
+      </ScrollView> 
     );
   }
 }
